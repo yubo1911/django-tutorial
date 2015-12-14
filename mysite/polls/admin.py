0a6b979a4dc1043303cc27d7d83a459f20d2eb63
Yubo home <yubo1911@163.com>
2015-12-14 04:43:26 -0800

    Add fieldset to admin page.

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from polls.models import Question
 
 class QuestionAdmin(admin.ModelAdmin):
-	fields = ['pub_date', 'question_text']
-
+	fieldsets = [
+		(None, {'fields': ['question_text']}),
+		('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+	]
 admin.site.register(Question, QuestionAdmin)
 
