5a862a56414b0a71842d89460d2d4d2c3bd133cb
Yubo home <yubo1911@163.com>
2015-12-14 04:58:08 -0800

    Add filters, search boxes to the admin page.

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -10,6 +10,10 @@ class QuestionAdmin(admin.ModelAdmin):
 		(None, {'fields': ['question_text']}),
 		('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
 	]
+
+	list_display = ('question_text', 'pub_date', 'was_published_recently')
+	list_filter = ['pub_date']
+	search_fields = ['question_text']
 	
 	inlines = [ChoiceInline]
 admin.site.register(Question, QuestionAdmin)
