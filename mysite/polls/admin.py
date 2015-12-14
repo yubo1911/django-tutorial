01514cce663881fa0dcf0762b0e6b2258b8dea0a
Yubo home <yubo1911@163.com>
2015-12-14 04:01:18 -0800

    Make the pub_date in front of question_text

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from polls.models import Question
 
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+	fields = ['pub_date', 'question_text']
+
+admin.site.register(Question, QuestionAdmin)
 
