fd181be724bb1345e057e0f30d12600baee5df30
Yubo home <yubo1911@163.com>
2015-12-16 05:12:23 -0800

    Use generic view to use less code

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,8 +4,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.index, name='index'),
-	url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
-	url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
-	url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+	url(r'^$', views.IndexView.as_view(), name='index'),
+	url(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name='detail'),
+	url(r'^(?P<pk>\d+)/results/$', views.ResultsView.as_view(), name='results'),
+	url(r'^(?P<question_id>\d+)/vote/$', views.vote, name='vote'),
 ]
