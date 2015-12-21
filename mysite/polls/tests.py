1c9cb8e1b998feb5e0f4d2b7b6c57a30606b0c9c
Yubo home <yubo1911@163.com>
2015-12-21 05:53:56 -0800

    Add more test cases.

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -16,5 +16,15 @@ class QuestionMethodTests(TestCase):
 		future_question = Question(pub_date=time)
 		self.assertEqual(future_question.was_published_recently(), False)
 
+	def test_was_published_recently_with_old_question(self):
+		time = timezone.now() - datetime.timedelta(days=30)
+		old_question = Question(pub_date=time)
+		self.assertEqual(old_question.was_published_recently(), False)
+	
+	def test_was_published_recently_with_recent_question(self):
+		time = timezone.now() - datetime.timedelta(hours=1)
+		recent_question = Question(pub_date=time)
+		self.assertEqual(recent_question.was_published_recently(), True)
+
 
 
